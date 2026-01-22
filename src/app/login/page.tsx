32975d9c929bfc726b7bcd6bea3ca1beffc1b06e
Thanaphon Noilomtuan <sahadieselkolkarn@gmail.com>
2026-01-22 05:35:32 +0000

    เปลี่ยนรูป back ground : https://firebasestorage.googleapis.com/v0/b/stu

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 // src/app/login/page.tsx
 'use client';
 
@@ -61,7 +62,7 @@ export default function LoginPage() {
     <div 
         className="flex min-h-screen items-center justify-center p-4"
         style={{
-            backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/studio-9091954631-7f4e0.firebasestorage.app/o/OPEC_Image.jpg?alt=media&token=b76e6822-bdc9-42b5-b0c1-49d445fbf9b4')",
+            backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/studio-9091954631-7f4e0.firebasestorage.app/o/OPEC_Image_2.png?alt=media&token=e0595de1-495c-4e2f-8c7f-79813a6d8519')",
             backgroundSize: 'contain',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
